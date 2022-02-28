6614e0082ef3648c4ec36213c6d708a644fae786
BiAniTy <drayttc@gmail.com>
2022-02-28 11:09:53 -0300

    Complete function to play x rounds and return final message

--- js/rps.js
+++ js/rps.js
@@ -17,6 +17,11 @@ function computer_play() {
     }
 }
 
+// Prompts the user for an option
+function player_play() {
+    return prompt("You must choose");
+}
+
 // Gets a random int between 0 and num(exclusive)
 function random_int(num) {
     return Math.floor(Math.random() * num);
@@ -29,9 +34,7 @@ function play_round() {
 
     console.log(computer_choice);
 
-    let player_choice = prompt("You must choose!");
-
-    player_choice = player_choice.toLowerCase();
+    let player_choice = player_play().toLowerCase();
 
     console.log(player_choice);
 
@@ -67,9 +70,13 @@ function play_round() {
                 case "paper":
                     return results(player_choice, computer_choice, true);
             }
+
+        default:
+            return "Problem with the play_round() function";
     }
 }
 
+// Returns a message with the result of the round
 function results(player_choice, computer_choice, player_win) {
     // Uppercases the first letter of each choice
     computer_choice =
@@ -88,4 +95,49 @@ function results(player_choice, computer_choice, player_win) {
     }
 }
 
-console.log(play_round());
+// Scores the game and returns a final result after n rounds
+function game() {
+    let player_score = 0;
+    let computer_score = 0;
+
+    // Changes the score depending on the winner
+    for (let i = 0; i < 5; i++) {
+        let result = play_round();
+
+        switch (result.substr(4, 4)) {
+            case "won!":
+                player_score++;
+                console.log(result);
+                break;
+
+            case "lost":
+                computer_score++;
+                console.log(result);
+                break;
+
+            case " a t":
+                console.log(result);
+                break;
+
+            default:
+                return "problem with the game() function";
+        }
+    }
+
+    // Displays the final result message
+    switch (true) {
+        case player_score === computer_score:
+            return `A tie!, you've won ${player_score} times out of 5!!!`;
+
+        case player_score > computer_score:
+            return `Congrats, you've won ${player_score} times out of 5!!!`;
+
+        case player_score < computer_score:
+            return `What a shame, you've lost ${computer_score} times out of 5.`;
+
+        default:
+            return "Problem with the game() function";
+    }
+}
+
+console.log(game());
