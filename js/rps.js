22626a6a0c8c567074f15537cd20360eb9e5611e
BiAniTy <drayttc@gmail.com>
2022-03-02 17:10:54 -0300

    Complete js functions

--- js/rps.js
+++ js/rps.js
@@ -18,9 +18,9 @@ function computer_play() {
 }
 
 // Prompts the user for an option
-function player_play() {
-    return prompt("You must choose");
-}
+// function player_play() {
+//     return prompt("You must choose");
+// }
 
 // Gets a random int between 0 and num(exclusive)
 function random_int(num) {
@@ -28,47 +28,67 @@ function random_int(num) {
 }
 
 // Simulates a round
-function play_round() {
+function play_round(player_choice) {
     // Determines the player and the computer choice
     let computer_choice = computer_play().toLowerCase();
 
-    console.log(computer_choice);
+    player.textContent = `You: ${player_choice}`;
+    comp.textContent = `Computer: ${computer_choice}`;
 
-    let player_choice = player_play().toLowerCase();
-
-    console.log(player_choice);
+    let res;
 
     // Return the results after comparing the choices
     switch (true) {
         case player_choice === computer_choice:
-            return "It's a tie!";
+            let result = "It's a tie!";
+            result_round.textContent = result;
+            choices.appendChild(result_round);
+            return result;
 
         // Depending on the player's choice, compare with the computer's and call the results() function
         case player_choice === "rock":
             switch (computer_choice) {
                 case "scissors":
-                    return results(player_choice, computer_choice, true);
+                    res = results(player_choice, computer_choice, true);
+                    result_round.textContent = res;
+                    choices.appendChild(result_round);
+                    return res;
 
                 case "paper":
-                    return results(player_choice, computer_choice, false);
+                    res = results(player_choice, computer_choice, false);
+                    result_round.textContent = res;
+                    choices.appendChild(result_round);
+                    return res;
             }
 
         case player_choice === "paper":
             switch (computer_choice) {
                 case "scissors":
-                    return results(player_choice, computer_choice, false);
+                    res = results(player_choice, computer_choice, false);
+                    result_round.textContent = res;
+                    choices.appendChild(result_round);
+                    return res;
 
                 case "rock":
-                    return results(player_choice, computer_choice, true);
+                    res = results(player_choice, computer_choice, true);
+                    result_round.textContent = res;
+                    choices.appendChild(result_round);
+                    return res;
             }
 
         case player_choice === "scissors":
             switch (computer_choice) {
                 case "rock":
-                    return results(player_choice, computer_choice, false);
+                    res = results(player_choice, computer_choice, false);
+                    result_round.textContent = res;
+                    choices.appendChild(result_round);
+                    return res;
 
                 case "paper":
-                    return results(player_choice, computer_choice, true);
+                    res = results(player_choice, computer_choice, true);
+                    result_round.textContent = res;
+                    choices.appendChild(result_round);
+                    return res;
             }
 
         default:
@@ -96,48 +116,120 @@ function results(player_choice, computer_choice, player_win) {
 }
 
 // Scores the game and returns a final result after n rounds
-function game() {
+function game(player_choice) {
     let player_score = 0;
     let computer_score = 0;
+    let rounds = 0;
 
     // Changes the score depending on the winner
-    for (let i = 0; i < 5; i++) {
-        let result = play_round();
+    let result = play_round(player_choice);
 
-        switch (result.substr(4, 4)) {
-            case "won!":
-                player_score++;
-                console.log(result);
-                break;
+    // console.log(result);
 
-            case "lost":
-                computer_score++;
-                console.log(result);
-                break;
+    switch (result.substr(4, 4)) {
+        case "won!":
+            rounds++;
+            player_score++;
+            break;
 
-            case " a t":
-                console.log(result);
-                break;
+        case "lost":
+            computer_score++;
+            rounds++;
+            break;
 
-            default:
-                return "problem with the game() function";
-        }
-    }
-
-    // Displays the final result message
-    switch (true) {
-        case player_score === computer_score:
-            return `A tie!, you've won ${player_score} times out of 5!!!`;
-
-        case player_score > computer_score:
-            return `Congrats, you've won ${player_score} times out of 5!!!`;
-
-        case player_score < computer_score:
-            return `What a shame, you've lost ${computer_score} times out of 5.`;
+        case " a t":
+            break;
 
         default:
-            return "Problem with the game() function";
+            return "problem with the game() function";
+    }
+
+    return {
+        computer_score: computer_score,
+        player_score: player_score,
+        rounds: rounds,
+    };
+}
+
+let player_score = 0;
+let computer_score = 0;
+let rounds = 0;
+
+function get_choice(e) {
+    let scores = game(e.target.textContent.toLowerCase());
+    player_score += scores.player_score;
+    computer_score += scores.computer_score;
+    rounds += scores.rounds;
+
+    scr.style["display"] = "initial";
+
+    c_score.textContent = `Computer: ${computer_score}`;
+    p_score.textContent = `You: ${player_score}`;
+
+    // Displays the final result message
+    if (player_score === 5 || computer_score === 5) {
+        switch (true) {
+            case player_score === computer_score:
+                final_result.textContent = `A tie!, you've won ${player_score} times out of ${rounds}!!!`;
+                //return `A tie!, you've won ${player_score} times out of ${rounds}!!!`;
+                choices.appendChild(final_result);
+                player_score = 0;
+                computer_score = 0;
+                rounds = 0;
+                return;
+
+            case player_score > computer_score:
+                final_result.textContent = `Congrats, you've won ${player_score} times out of ${rounds}!!!`;
+                // return `Congrats, you've won ${player_score} times out of ${rounds}!!!`;
+                choices.appendChild(final_result);
+                player_score = 0;
+                computer_score = 0;
+                rounds = 0;
+                return;
+
+            case player_score < computer_score:
+                final_result.textContent = `What a shame, you've lost ${computer_score} times out of ${rounds}.`;
+                // return `What a shame, you've lost ${computer_score} times out of ${rounds}.`;
+                choices.appendChild(final_result);
+                player_score = 0;
+                computer_score = 0;
+                rounds = 0;
+                return;
+
+            default:
+                return "Problem with the game() function";
+        }
+    } else if (choices.contains(final_result)) {
+        choices.removeChild(final_result);
     }
 }
 
-console.log(game());
+const choices = document.querySelector(".choices");
+const scr = document.querySelector(".scores");
+
+const final_result = document.createElement("div");
+const result_round = document.createElement("div");
+
+const player = document.createElement("div");
+const comp = document.createElement("div");
+choices.appendChild(player);
+choices.appendChild(comp);
+
+const p_score = document.createElement("div");
+scr.appendChild(p_score);
+
+const c_score = document.createElement("div");
+scr.appendChild(c_score);
+
+const rock = document.querySelector(".rock");
+rock.addEventListener("click", get_choice);
+
+// const scissors = document.querySelector(".scissors");
+// scissors.addEventListener("click", play_round(scissors, computer_play));
+
+// const paper = document.querySelector(".paper");
+// paper.addEventListener("click", play_round(paper, computer_play));
+
+// console.log(rock, paper, scissors);
+
+// console.log(game());
